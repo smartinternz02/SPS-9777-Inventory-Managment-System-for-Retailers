9251bc02193da5d6208f30c5b58e2cb7c66bba66
Shreevathsav <shreevathsavsuresh@gmail.com>
2021-04-21 11:12:48 +0530

    First commit

---
+++ src/test/java/com/futuretech/inventorymanagementapp/InventoryManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futuretech.inventorymanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
